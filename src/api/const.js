a72f809dc5742abecdabe1c0d7f05a20df352939
OksanaPod31 <aksanapodkovyrova@yandex.ru>
2022-11-11 04:48:28 +0300

    fAPI

---
+++ src/api/const.js
@@ -0,0 +1,7 @@
+export const URL = 'https://oauth.reddit.com';
+export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
+export const CLIENT_ID = 'bU8WQevygLJEGzxP4C_y0A';
+export const RESPONCE_TYPE = 'token';
+export const RANDOM_STRING = 'rand';
+export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const SCOPE = 'identity submit read';
--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import {useEffect, React, useState} from 'react';
 import style from './Auth.module.css';
 import PropTypes from 'prop-types';
 import {ReactComponent as AuthImg} from './img/login.svg';
+import {urlAuth} from '../../../api/auth';
+import {Text} from '../../../UI/Text';
+import {URL} from '../../../api/const';
 
-export const Auth = ({auth}) => (
-  <button className={style.button}>
-    {auth ? auth :
-      <AuthImg className={style.svg} />
-    }
-  </button>
-);
+
+export const Auth = ({token}) => {
+  const [auth, setAuth] = useState({});
+
+
+  useEffect(() => {
+    if (!token) return;
+    console.log(token);
+
+    fetch(`${URL}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    }).then(responce => responce.json()
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?*$/, '');
+        setAuth({name, img});
+      }));
+  }, [token]);
+
+  return (
+    <div className={style.container}>
+      {auth.name ?
+      (<img src={auth.img} title={auth.name} alt={`Аватар: ${auth.name}`}/>) :
+      (
+        <Text As='a' href={urlAuth}>
+          <AuthImg className={style.svg} />
+        </Text>
+      )
+      }
+    </div>
+  );
+};
 
 Auth.propTypes = {
-  auth: PropTypes.bool
+  token: PropTypes.string
 };
 
